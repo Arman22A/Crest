3309c37fd5df7946fa4ff713a91050c4abd69cfc
Arman22A <armastt71@gmail.com>
2026-08-07 12:22:09 +0300

    Refresh Crest content studio cache

--- sw.js
+++ sw.js
@@ -1,11 +1,11 @@
-const cacheName = "crest-v36";
+const cacheName = "crest-v37";
 const vapidPublicKey = "BA1j44cNJV6QoirknYZOiFPQaLiygwxyVmRbaFCcIm3V5lFmTeM-S1SgctoZXNNR5makhB7ip44OcXjDXNMeRQc";
 const assets = [
   "./",
   "./index.html",
-  "./styles.css?v=36",
+  "./styles.css?v=37",
   "./vendor/supabase-2.110.3.js?v=30",
-  "./script.js?v=36",
+  "./script.js?v=37",
   "./manifest.webmanifest?v=30",
   "./icon.ico?v=30",
   "./icon-192.png",
