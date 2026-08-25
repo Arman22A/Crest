1e4e6ac10361282e3a9aefdd74d13742ac8cdc7e
Arman22A <armastt71@gmail.com>
2026-08-25 21:25:58 +0300

    Fix consecutive calendar streaks (v42): sw.js

--- sw.js
+++ sw.js
@@ -1,11 +1,11 @@
-const cacheName = "crest-v41";
+const cacheName = "crest-v42";
 const vapidPublicKey = "BA1j44cNJV6QoirknYZOiFPQaLiygwxyVmRbaFCcIm3V5lFmTeM-S1SgctoZXNNR5makhB7ip44OcXjDXNMeRQc";
 const assets = [
   "./",
   "./index.html",
-  "./styles.css?v=41",
+  "./styles.css?v=42",
   "./vendor/supabase-2.110.3.js?v=30",
-  "./script.js?v=41",
+  "./script.js?v=42",
   "./manifest.webmanifest?v=30",
   "./icon.ico?v=30",
   "./icon-192.png",
